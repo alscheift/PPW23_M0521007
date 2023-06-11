a18d0c44b8a0fba86d222838e3649965400815ea
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-11 08:52:48 +0700

    DbSeed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,17 +26,53 @@ class DatabaseSeeder extends Seeder
             'username' => 'meizaacr'
         ]);
 
+        $user2 = User::factory()->create([
+            'name' => 'Avilio',
+            'username' => '4v1l1o'
+        ]);
+
         $category1 = Category::factory()->create([
             'name' => 'Personal',
             'slug' => 'personal',
+        ]);
+
+        $category2 = Category::factory()->create([
+            'name' => 'Hobbies',
+            'slug' => 'hobbies',
+        ]);
+
+        $category3 = Category::factory()->create([
+            'name' => 'Work',
+            'slug' => 'work',
         ]);
 
         Post::factory(5)->create([
             'user_id' => $user1->id,
             'category_id' => $category1->id,
         ]);
+        Post::factory(5)->create([
+            'user_id' => $user1->id,
+            'category_id' => $category2->id,
+        ]);
 
-        Post::factory(10)->create();
+        Post::factory(5)->create([
+            'user_id' => $user2->id,
+            'category_id' => $category1->id,
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user2->id,
+            'category_id' => $category2->id,
+        ]);
+        Post::factory(2)->create([
+            'user_id' => $user1->id,
+            'category_id' => $category3->id,
+        ]);
+        Post::factory(3)->create([
+            'user_id' => $user2->id,
+            'category_id' => $category3->id,
+        ]);
+
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
