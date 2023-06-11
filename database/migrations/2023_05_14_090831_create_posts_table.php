69ed1172aeb065ef5df5dea16f5286e7dd9ea20d
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-11 13:44:19 +0700

    'comment'

--- database/migrations/2023_05_14_090831_create_posts_table.php
+++ database/migrations/2023_05_14_090831_create_posts_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,8 +13,8 @@ return new class extends Migration {
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete(); // this is the same with $table->unsignedBigInteger('user_id');
-            $table->foreignId('category_id'); // this is the same with $table->unsignedBigInteger('category_id');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id');
             $table->string('slug')->unique();
             $table->string('title');
             $table->string('thumbnail')->nullable();
