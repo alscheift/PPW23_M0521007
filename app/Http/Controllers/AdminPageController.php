16840dacd298aca2dca708fcd1d1d756d4955e33
Afif Nur Fauzi <alscheift@gmail.com>
2023-07-06 16:54:37 +0700

    AdminPage for posts

--- app/Http/Controllers/AdminPageController.php
+++ app/Http/Controllers/AdminPageController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminPageController extends Controller
 {
     public function index()
     {
-        $url = (new Request)->path();
+        $url = \request()->path();
         $segments = explode('/', $url);
+        $items = [];
 
         // Get the value after "admin/"
-        $posts = $segments[1];
+        $var = $segments[1];
 
-        return view('admin.index');
+        if ($var == 'posts') {
+            $items = Post::all();
+        } elseif ($var == 'users') {
+            $items = User::all();
+        } elseif ($var == 'comments') {
+            $items = Comment::all();
+        }
+        return view('admin.index', compact('items', 'var'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,37 @@
 <x-layout>
     <x-setting heading="Admin Page" route="admin">
         <table class="min-w-full divide-y divide-gray-200">
+            <tbody class="bg-white divide-y divide-gray-200">
+            @foreach($items as $item)
+                @if($var == 'posts')
+                    <tr>
+                        <td class="px-6 py-4">
+                            <a class="text-s" href="/posts/{{$item->slug}}">
+                                {{ $item->title}}
+                            </a>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <a class="text-s text-blue-400" href="/user/posts/{{$item->slug}}/edit">
+                                Edit
+                            </a>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <form action="/user/posts/{{$item->slug}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="text-s text-red-400" type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @elseif($var=='users')
 
+                @elseif($var=='comments')
+                @else
+
+                @endif
+            @endforeach
+
+            </tbody>
         </table>
         <div class="mt-8 flex flex-col">
         </div>
