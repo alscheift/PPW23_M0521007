50ff2c5b12916aeb9f9ea7775ad65c1f5836b420
Afif Nur Fauzi <alscheift@gmail.com>
2023-07-07 20:46:03 +0700

    Gate (Admin)

--- app/Http/Middleware/AdminOnly.php
+++ app/Http/Middleware/AdminOnly.php
@@ -16,7 +16,7 @@ class AdminOnly
     public function handle(Request $request, Closure $next): Response
     {
         // if not admin
-        if (!auth()->user()->is_admin) {
+        if (!auth()->user()->can('admin')) {
             abort(403);
         }
 
