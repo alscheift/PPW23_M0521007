62e94fbac3c5b74e9f8ecadde75306c77e69c661
Afif Nur Fauzi <alscheift@gmail.com>
2023-07-07 21:52:01 +0700

    Error flash message

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -5,4 +5,11 @@
     >
         <p class="fixed right-0 bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm">{{session('success')}}</p>
     </div>
+@elseif(session()->has('error'))
+    <div x-data="{ show: true }"
+         x-init="setTimeout(() => show = false, 4000)"
+         x-show="show"
+    >
+        <p class="fixed right-0 bg-red-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm">{{session('error')}}</p>
+    </div>
 @endif
